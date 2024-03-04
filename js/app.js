d66e5586fef80236d86feb173c78db44c6a8ccda
cshermam <138785055+cshermam@users.noreply.github.com>
2024-03-04 10:09:16 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn-login");
+
+btn.addEventListener("click", () => {
+  //   location.href = "../pages/cep2.html";
+  window.location = "../pages/cep2.html";
+});
---
+++ js/cadastro.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn-cadastro");
+
+btn.addEventListener("click", () => {
+  //   location.href = "../pages/cep2.html";
+  window.location = "../index.html";
+});
